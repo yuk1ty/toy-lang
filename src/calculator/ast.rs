6b82b9b13600ebe37060d45e669521914ba716dc
yuk1ty <yuki.mul.tiplus@gmail.com>
2021-10-30 13:35:21 +0900

    add variable, assignment, identifier

--- src/calculator/ast.rs
+++ src/calculator/ast.rs
@@ -1,6 +1,6 @@
 use std::fmt::Debug;
 
-pub fn add(lhs: Expression, rhs: Expression) -> Expression {
+pub fn add<'a>(lhs: Expression<'a>, rhs: Expression<'a>) -> Expression<'a> {
     Expression::BinaryExpression {
         operator: Operator::Add,
         lhs: Box::new(lhs),
@@ -8,7 +8,7 @@ pub fn add(lhs: Expression, rhs: Expression) -> Expression {
     }
 }
 
-pub fn subtract(lhs: Expression, rhs: Expression) -> Expression {
+pub fn subtract<'a>(lhs: Expression<'a>, rhs: Expression<'a>) -> Expression<'a> {
     Expression::BinaryExpression {
         operator: Operator::Subtract,
         lhs: Box::new(lhs),
@@ -16,7 +16,7 @@ pub fn subtract(lhs: Expression, rhs: Expression) -> Expression {
     }
 }
 
-pub fn multiply(lhs: Expression, rhs: Expression) -> Expression {
+pub fn multiply<'a>(lhs: Expression<'a>, rhs: Expression<'a>) -> Expression<'a> {
     Expression::BinaryExpression {
         operator: Operator::Multiply,
         lhs: Box::new(lhs),
@@ -24,7 +24,7 @@ pub fn multiply(lhs: Expression, rhs: Expression) -> Expression {
     }
 }
 
-pub fn divide(lhs: Expression, rhs: Expression) -> Expression {
+pub fn divide<'a>(lhs: Expression<'a>, rhs: Expression<'a>) -> Expression<'a> {
     Expression::BinaryExpression {
         operator: Operator::Divide,
         lhs: Box::new(lhs),
@@ -32,18 +32,34 @@ pub fn divide(lhs: Expression, rhs: Expression) -> Expression {
     }
 }
 
-pub fn integer(value: i32) -> Expression {
+pub fn integer<'a>(value: i32) -> Expression<'a> {
     Expression::IntegerLiteral(value)
 }
 
+pub fn assignment<'a>(name: &'a str, expression: Expression<'a>) -> Expression<'a> {
+    Expression::Assignment {
+        name,
+        expression: Box::new(expression),
+    }
+}
+
+pub fn identifier<'a>(name: &'a str) -> Expression<'a> {
+    Expression::Identifier(name)
+}
+
 #[derive(PartialEq, Debug)]
-pub enum Expression {
+pub enum Expression<'a> {
     BinaryExpression {
         operator: Operator,
-        lhs: Box<Expression>,
-        rhs: Box<Expression>,
+        lhs: Box<Expression<'a>>,
+        rhs: Box<Expression<'a>>,
     },
     IntegerLiteral(i32),
+    Assignment {
+        name: &'a str,
+        expression: Box<Expression<'a>>,
+    },
+    Identifier(&'a str),
 }
 
 #[derive(PartialEq, Debug)]
--- src/calculator/interpreter.rs
+++ src/calculator/interpreter.rs
@@ -1,18 +1,41 @@
+use std::collections::HashMap;
+
 use crate::calculator::ast::{Expression, Operator};
 
-pub fn interpret(expression: Expression) -> i32 {
-    match expression {
-        Expression::BinaryExpression { operator, lhs, rhs } => {
-            let lhs = interpret(*lhs);
-            let rhs = interpret(*rhs);
-            match operator {
-                Operator::Add => lhs + rhs,
-                Operator::Subtract => lhs - rhs,
-                Operator::Multiply => lhs * rhs,
-                Operator::Divide => lhs / rhs,
+pub struct Interpreter<'a> {
+    environment: HashMap<&'a str, i32>,
+}
+
+impl<'a> Interpreter<'a> {
+    pub fn new() -> Self {
+        Self {
+            environment: HashMap::new(),
+        }
+    }
+
+    pub fn interpret(&mut self, expression: Expression<'a>) -> i32 {
+        match expression {
+            Expression::BinaryExpression { operator, lhs, rhs } => {
+                let lhs = self.interpret(*lhs);
+                let rhs = self.interpret(*rhs);
+                match operator {
+                    Operator::Add => lhs + rhs,
+                    Operator::Subtract => lhs - rhs,
+                    Operator::Multiply => lhs * rhs,
+                    Operator::Divide => lhs / rhs,
+                }
+            }
+            Expression::IntegerLiteral(value) => value,
+            Expression::Identifier(ident) => *self
+                .environment
+                .get(ident)
+                .expect(&format!("Undefined variable: {}", ident)),
+            Expression::Assignment { name, expression } => {
+                let value = self.interpret(*expression);
+                self.environment.insert(name, value);
+                value
             }
         }
-        Expression::IntegerLiteral(value) => value,
     }
 }
 
@@ -21,76 +44,117 @@ mod test {
     use super::*;
     use crate::calculator::ast::*;
 
+    fn interpreter<'a>() -> Interpreter<'a> {
+        Interpreter::new()
+    }
+
     #[test]
     fn test_10_plus_20_should_work() {
         let e = add(integer(10), integer(20));
-        assert_eq!(30, interpret(e));
+        assert_eq!(30, interpreter().interpret(e));
     }
 
     #[test]
     fn test_10_plus_0_should_work() {
         let e = add(integer(10), integer(0));
-        assert_eq!(10, interpret(e));
+        assert_eq!(10, interpreter().interpret(e));
     }
 
     #[test]
     fn test_0_plus_10_should_work() {
         let e = add(integer(0), integer(10));
-        assert_eq!(10, interpret(e));
+        assert_eq!(10, interpreter().interpret(e));
     }
 
     #[test]
     fn test_10_minus_20_should_work() {
         let e = subtract(integer(10), integer(20));
-        assert_eq!(-10, interpret(e));
+        assert_eq!(-10, interpreter().interpret(e));
     }
 
     #[test]
     fn test_10_minus_0_should_work() {
         let e = subtract(integer(10), integer(0));
-        assert_eq!(10, interpret(e));
+        assert_eq!(10, interpreter().interpret(e));
     }
 
     #[test]
     fn test_0_minus_10_should_work() {
         let e = subtract(integer(0), integer(10));
-        assert_eq!(-10, interpret(e));
+        assert_eq!(-10, interpreter().interpret(e));
     }
 
     #[test]
     fn test_10_multiply_20_should_work() {
         let e = multiply(integer(10), integer(20));
-        assert_eq!(200, interpret(e));
+        assert_eq!(200, interpreter().interpret(e));
     }
 
     #[test]
     fn test_10_multiply_0_should_work() {
         let e = multiply(integer(10), integer(0));
-        assert_eq!(0, interpret(e));
+        assert_eq!(0, interpreter().interpret(e));
     }
 
     #[test]
     fn test_0_multiply_10_should_work() {
         let e = multiply(integer(0), integer(10));
-        assert_eq!(0, interpret(e));
+        assert_eq!(0, interpreter().interpret(e));
     }
 
     #[test]
     fn test_20_divide_10_should_work() {
         let e = divide(integer(20), integer(10));
-        assert_eq!(2, interpret(e));
+        assert_eq!(2, interpreter().interpret(e));
     }
 
     #[test]
     #[should_panic]
     fn test_10_divide_0_should_work() {
         let e = divide(integer(10), integer(0));
-        interpret(e);
+        interpreter().interpret(e);
     }
 
     #[test]
     fn test_0_divide_10_should_work() {
         let e = divide(integer(0), integer(10));
-        assert_eq!(0, interpret(e));
+        assert_eq!(0, interpreter().interpret(e));
+    }
+
+    #[test]
+    fn test_assignment_should_work() {
+        let e = assignment("a", add(integer(10), integer(10)));
+        let mut interpreter = interpreter();
+        assert_eq!(20, interpreter.interpret(e));
+        assert_eq!(20, *interpreter.environment.get("a").unwrap());
+    }
+
+    #[test]
+    fn test_identifier_after_assignment_should_work() {
+        let mut interpreter = interpreter();
+        let e1 = assignment("a", add(integer(10), integer(10)));
+        assert_eq!(20, interpreter.interpret(e1));
+        let e2 = add(identifier("a"), integer(10));
+        assert_eq!(30, interpreter.interpret(e2));
+    }
+
+    #[test]
+    fn test_increment_should_work() {
+        let mut interpreter = interpreter();
+        let e1 = assignment("a", add(integer(10), integer(10)));
+        assert_eq!(20, interpreter.interpret(e1));
+        let e2 = add(identifier("a"), integer(10));
+        assert_eq!(30, interpreter.interpret(e2));
+        assert_eq!(30, *interpreter.environment.get("a").unwrap());
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_identifier_not_found() {
+        let mut interpreter = interpreter();
+        let e1 = assignment("a", add(integer(10), integer(10)));
+        assert_eq!(20, interpreter.interpret(e1));
+        let e2 = add(identifier("b"), integer(10));
+        interpreter.interpret(e2);
     }
 }
